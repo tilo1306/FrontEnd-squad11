f14549a92988b04fe85b519b0606cbf06421b842
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-02 14:30:37 -0300

    ajust

--- src/functions/formToJson.ts
+++ src/functions/formToJson.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export function formToJSON(formElement: any) {
   const formData = new FormData(formElement)
-  const data: { [key: string]: any } = {} // Adicionando uma anotação de tipo
-
+  const data: { [key: string]: any } = {}
   for (const [key, value] of formData.entries()) {
     data[key] = value
   }
